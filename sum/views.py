85d5f8f150f157f7ce121498e74260bbe94c5a7a
adarshjha17 <ajprogrammer17@gmail.com>
2019-10-15 04:22:46 +0530

    include onclick for add, sub, mul, div

--- sum/views.py
+++ sum/views.py
@@ -5,6 +5,18 @@ from django.shortcuts import render
 def index(request):
 	a = request.POST.get('txt1',False)
 	b = request.POST.get('txt2',False)
-	c = int(a)+int(b)
-	context = {'sum':c}
+	context = {}
+	if request.POST.get('add',False):
+		c = int(a)+int(b)
+		context = {'sum':c}		
+	elif request.POST.get('sub',False):
+		c = int(a)-int(b)
+		context = {'sum':c}	
+	elif request.POST.get('mul',False):
+		c = int(a)*int(b)
+		context = {'sum':c}	
+	elif request.POST.get('div',False):
+		c = int(a)/int(b)
+		context = {'sum':c}	
+	
 	return render(request,'sum/index.html',context)
